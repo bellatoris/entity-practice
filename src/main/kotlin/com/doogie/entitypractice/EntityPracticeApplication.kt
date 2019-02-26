b540fcd5e6c2e9d1bf97bd5424c53eef6fce9a82
bellatoris <bellatoris@snu.ac.kr>
2019-02-26 10:07:45 +0900

    complete 2.5.7

--- src/main/kotlin/com/doogie/entitypractice/EntityPracticeApplication.kt
+++ src/main/kotlin/com/doogie/entitypractice/EntityPracticeApplication.kt
@@ -4,14 +4,17 @@ import com.doogie.differenet.DifferentPackageDoogie
 import com.doogie.differenet.DifferentPackageDoogieRepository
 import com.doogie.entitypractice.low.LowPackageDoogie
 import com.doogie.entitypractice.low.LowPackageDoogieRepository
+import com.doogie.entitypractice.model.Book
+import org.hibernate.testing.transaction.TransactionUtil.doInJPA
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.JpaContext
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.support.TransactionTemplate
 import javax.persistence.Entity
+import javax.persistence.EntityManagerFactory
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
@@ -36,34 +39,46 @@ interface LowLevelDoogieRepository : JpaRepository<EntityPracticeApplication.Low
 
 fun main(args: Array<String>) {
     runApplication<EntityPracticeApplication>(*args).use {
-        val doogie = Doogie.getInstance()
-        val transactionTemplate = it
-            .beanFactory
-            .getBean(TransactionTemplate::class.java)
-        val em = it
-            .beanFactory
-            .getBean(JpaContext::class.java)
-            .getEntityManagerByManagedType(Doogie::class.java)
-
-        transactionTemplate.execute { _ ->
-            val savedDoogie = it.getBean(DoogieRepository::class.java).save(doogie)
-            println(savedDoogie)
-        }
-        transactionTemplate.execute { _ ->
-            val getDoogie = it.getBean(DoogieRepository::class.java).getOne(1)
-            println(getDoogie)
-        }
-
-        val lowLevelDoogie = EntityPracticeApplication.LowLevelDoogie()
-        val savedLowLevelDoogie = it.getBean(LowLevelDoogieRepository::class.java).save(lowLevelDoogie)
-        println(savedLowLevelDoogie)
-
-        val lowPackageDoogie = LowPackageDoogie.getInstance()
-        val savedLowPackageDoogie = it.getBean(LowPackageDoogieRepository::class.java).save(lowPackageDoogie)
-        println(savedLowPackageDoogie)
-
-        val differentPackageDoogie = DifferentPackageDoogie.getInstance()
-        val savedDifferentPackageDoogie = it.getBean(DifferentPackageDoogieRepository::class.java).save(differentPackageDoogie)
-        println(savedDifferentPackageDoogie)
+        testPojo(it)
+        testMapping(it)
     }
+}
+
+private fun testMapping(it: ConfigurableApplicationContext) {
+    doInJPA({ it.beanFactory.getBean(EntityManagerFactory::class.java) }) { em ->
+        val book = Book()
+        em.persist(book)
+        em.flush()
+        val book1 = em.find(Book::class.java, 1L)
+        val book2 = em.find(Book::class.java, 1L)
+        assert(book1 == book2)
+    }
+}
+
+private fun testPojo(it: ConfigurableApplicationContext) {
+    val doogie = Doogie.getInstance()
+    val transactionTemplate = it
+        .beanFactory
+        .getBean(TransactionTemplate::class.java)
+
+    transactionTemplate.execute { _ ->
+        val savedDoogie = it.getBean(DoogieRepository::class.java).save(doogie)
+        println(savedDoogie)
+    }
+    transactionTemplate.execute { _ ->
+        val getDoogie = it.getBean(DoogieRepository::class.java).getOne(1)
+        println(getDoogie)
+    }
+
+    val lowLevelDoogie = EntityPracticeApplication.LowLevelDoogie()
+    val savedLowLevelDoogie = it.getBean(LowLevelDoogieRepository::class.java).save(lowLevelDoogie)
+    println(savedLowLevelDoogie)
+
+    val lowPackageDoogie = LowPackageDoogie.getInstance()
+    val savedLowPackageDoogie = it.getBean(LowPackageDoogieRepository::class.java).save(lowPackageDoogie)
+    println(savedLowPackageDoogie)
+
+    val differentPackageDoogie = DifferentPackageDoogie.getInstance()
+    val savedDifferentPackageDoogie = it.getBean(DifferentPackageDoogieRepository::class.java).save(differentPackageDoogie)
+    println(savedDifferentPackageDoogie)
 }
